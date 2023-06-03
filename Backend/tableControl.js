9eb9c5c22b1e8d29a025940596dbc80c842226c6
Durval <pedrocruzeiro@ua.pt>
2023-06-03 21:13:17 +0100

    "no automatic SP cration"

--- Backend/tableControl.js
+++ Backend/tableControl.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import { runInContext } from 'vm';
 
 export default async function(bd) {
-  let filesInOrder = ['Drops', 'Tables', 'Inserts', 'Triggers', 'SPs'];
+  let filesInOrder = ['Drops', 'Tables', 'Inserts', 'Triggers'];
   for (let file of filesInOrder) {
     let sqlFile = fs.readFileSync('./sql/' + file + '.sql', 'utf-8');
     await bd.query(sqlFile);
